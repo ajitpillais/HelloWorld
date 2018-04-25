fa86ce065d2bd487b7e045fbadb62d76561f124f
devops <devops@devops-vm>
2018-04-25 11:01:20 -0400

    Added from office text in class

--- HelloWorld.JavaProj/src/HelloWorld.java
+++ HelloWorld.JavaProj/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to DevOps World on MAC OSX");
+		System.out.println("Welcome to DevOps World on MAC OSX from office");
 
 	}
 
